e3c6855888415a9cc316b509f22dc730ab269aa4
StasitStas <pugachstas0910@gmail.com>
2025-03-19 20:31:26 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+int main() {
+    printf("Welcome! This program performs basic arithmetic operations.\n");
+
+    int a = 5, b = 10;
+    printf("Sum: %d\n", add(a, b));
+    printf("Difference: %d\n", subtract(a, b));
+
+    return 0;
+}
